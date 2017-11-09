0bd05b3fa3ce7bb9434a67a7d142fa239e4ba269
Oliver Schnuck <oliver.schnuck@br.de>
2017-11-09 18:21:28 +0100

    Select city by command line argument

--- scrape.js
+++ scrape.js
@@ -3,7 +3,7 @@ var request = require('request'),
 
 var baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
   mongoUrl = 'mongodb://localhost:27017/obike',
-  collectionName = 'muenchen';
+  collectionName = process.argv[2] ? process.argv[2] : 'muenchen';
 
 
 (function init() {
@@ -18,23 +18,38 @@ var baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
 
       console.log('Connected to database');
 
-      var lat0 = 48.016406,
-        lon0 = 11.312153,
-        lat1 = 48.256832,
-        lon1 = 11.800645;
+      var lat0, lon0, lat1, lon1;
 
+      if (collectionName === 'frankfurt') {
+
+        lat0 = 49.984826;
+        lon0 = 8.413478;
+        lat1 = 50.239489;
+        lon1 = 8.856560;
+      } else
+      // if (collectionName === 'muenchen')
+      {
+
+        lat0 = 48.016406;
+        lon0 = 11.312153;
+        lat1 = 48.256832;
+        lon1 = 11.800645;
+      }
+
+      // count requests
       var i = 1;
+
+      // // count bikes (with overlappings)
       // var j = 0;
 
-      for (var lat = lat0; lat <= lat1; lat = lat + (1 / 110.574)) {
-
-        for (var lon = lon0; lon <= lon1; lon = lon + (1 / (111.320 * Math.cos(lat * (Math.PI / 180))))) {
+      for (var lat = lat0; lat <= lat1; lat = lat + (1 / 110.574) * 0.9) {
+        // for transformation of 1km to degree see https://stackoverflow.com/questions/1253499/simple-calculations-for-working-with-lat-lon-km-distance
+        for (var lon = lon0; lon <= lon1; lon = lon + (1 / (111.320 * Math.cos(lat * (Math.PI / 180)))) * 0.9) {
 
           var url = baseUrl + 'latitude=' + lat + '&longitude=' + lon;
-
           console.log(i + ': ' + url);
-          setTimeout(request, i * 100, url, {jar: true}, scrape);
 
+          setTimeout(request, i * 100, url, {jar: true}, scrape);
           i++;
         }
       }
@@ -50,8 +65,8 @@ var baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
 
         var bikes = JSON.parse(body).data.list;
         // j = j + bikes.length;
-        save(bikes, db);
 
+        save(bikes, db);
       } else {
 
         console.log('Request failed');
@@ -98,7 +113,6 @@ var baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
       });
 
       // console.log(i);
-
       if (i === 1) {
 
         console.log('Close database');
