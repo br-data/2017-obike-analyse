dcdb03fdffca5dc28b6eb082facfc3ce1641ca80
Oliver Schnuck <oliver.schnuck@br.de>
2017-11-10 17:42:33 +0100

    Handle cities in script

--- scrape.js
+++ scrape.js
@@ -3,14 +3,16 @@ var request = require('request'),
 
 var baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
   mongoUrl = 'mongodb://localhost:27017/obike',
-  collectionName = process.argv[2] ? process.argv[2] : 'muenchen';
+  collectionName = 'bikes',
+  cities = ['muenchen', 'frankfurt'];
 
 
 (function init() {
 
-  var datetime = new Date();
-  var date = datetime.getFullYear() + '-' + datetime.getMonth() + '-' + datetime.getDate();
-  var hour = datetime.getHours();
+  var datetime = new Date(),
+    date = datetime.getFullYear() + '-' + datetime.getMonth() + '-' + datetime.getDate(),
+    hour = datetime.getHours();
+    // city;
 
   mongoClient.connect(mongoUrl, function(error, db) {
 
@@ -20,37 +22,42 @@ var baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
 
       var lat0, lon0, lat1, lon1;
 
-      if (collectionName === 'frankfurt') {
-
-        lat0 = 49.984826;
-        lon0 = 8.413478;
-        lat1 = 50.239489;
-        lon1 = 8.856560;
-      } else
-      // if (collectionName === 'muenchen')
-      {
-
-        lat0 = 48.016406;
-        lon0 = 11.312153;
-        lat1 = 48.256832;
-        lon1 = 11.800645;
-      }
-
       // count requests
       var i = 1;
 
       // // count bikes (with overlappings)
-      // var j = 0;
+        // var j = 0;
 
-      for (var lat = lat0; lat <= lat1; lat = lat + (1 / 110.574) * 0.9) {
-        // for transformation of 1km to degree see https://stackoverflow.com/questions/1253499/simple-calculations-for-working-with-lat-lon-km-distance
-        for (var lon = lon0; lon <= lon1; lon = lon + (1 / (111.320 * Math.cos(lat * (Math.PI / 180)))) * 0.9) {
+      for (var z = 0; z < cities.length; z++) {
 
-          var url = baseUrl + 'latitude=' + lat + '&longitude=' + lon;
-          console.log(i + ': ' + url);
+        var city = cities[z];
 
-          setTimeout(request, i * 100, url, {jar: true}, scrape);
-          i++;
+        if (city === 'frankfurt') {
+
+          lat0 = 49.984826;
+          lon0 = 8.413478;
+          lat1 = 50.239489;
+          lon1 = 8.856560;
+        } else
+        // if (city === 'muenchen')
+        {
+
+          lat0 = 48.016406;
+          lon0 = 11.312153;
+          lat1 = 48.256832;
+          lon1 = 11.800645;
+        }
+
+        for (var lat = lat0; lat <= lat1; lat = lat + (1 / 110.574) * 0.9) {
+          // for transformation of 1km to degree see https://stackoverflow.com/questions/1253499/simple-calculations-for-working-with-lat-lon-km-distance
+          for (var lon = lon0; lon <= lon1; lon = lon + (1 / (111.320 * Math.cos(lat * (Math.PI / 180)))) * 0.9) {
+
+            var url = baseUrl + 'latitude=' + lat + '&longitude=' + lon;
+            console.log(city + ': ' + i + ': ' + url);
+
+            setTimeout(request, i * 100, url, {jar: true}, scrape);
+            i++;
+          }
         }
       }
     } else {
@@ -83,13 +90,15 @@ var baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
 
         element['date'] = date;
         element['hour'] = hour;
+        element['city'] = city;
 
         // console.log(element);
         collection.update(
           {
             id: element.id,
             date: date,
-            hour: hour
+            hour: hour,
+            city: city
           },
           element,
           { upsert: true, multi: false },
