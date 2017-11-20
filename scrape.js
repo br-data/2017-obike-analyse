bbdfc1aef94df98993b1c60fb4f8a7709c830cf0
Steffen Kühne <stekhn@gmail.com>
2017-11-20 17:10:48 +0100

    Split code into functions

--- scrape.js
+++ scrape.js
@@ -1,168 +1,170 @@
-var request = require('request'),
-  mongoClient = require('mongodb').MongoClient;
+const request = require('request');
+const mongoClient = require('mongodb').MongoClient;
 
-var baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
-  mongoUrl = 'mongodb://localhost:27017/obike',
-  collectionName = 'bikes',
-  // Add city (e.g. 'berlin') here
-  cities = ['muenchen', 'frankfurt'];
+const baseUrl = 'https://mobile.o.bike/api/v1/bike/list?',
+const mongoUrl = 'mongodb://localhost:27017/obike';
+const collectionName = 'bikes';
 
+// Add city (e.g. 'berlin') here
+const cities = ['muenchen', 'frankfurt'];
+
+const boundaries = {
+  frankfurt: {
+    lat0: 49.984826,
+    lon0: 8.4134782,
+    lat1: 50.239489,
+    lon1: 8.8565601
+  },
+  berlin: {
+    lat0: 52.380591,
+    lon0: 13.010205,
+    lat1: 52.678389,
+    lon1: 13.793848
+  },
+  muenchen: {
+    lat0: 48.016406,
+    lon0: 11.312153,
+    lat1: 48.256832,
+    lon1: 11.800645
+  }
+}
 
 (function init() {
 
-  var datetime = new Date(),
-    date = datetime.getFullYear() + '-' + datetime.getMonth() + '-' + datetime.getDate(),
-    hour = datetime.getHours(),
-    minute = datetime.getMinutes(),
-    city;
+  const datetime = new Date();
+  const date = `${datetime.getFullYear()}-${datetime.getMonth()}-${datetime.getDate()}`;
+  const hour = datetime.getHours();
+  const minute = datetime.getMinutes();
+
+  connect(prepare);
+})();
+
+function connect(callback) {
 
   // TODO: divide maths and db-connect
-  mongoClient.connect(mongoUrl, function(error, db) {
+  mongoClient.connect(mongoUrl, (error, db) => {
 
     if (!error) {
 
       console.log('Connected to database');
-
-      var lat0, lon0, lat1, lon1;
-
-      // count requests
-      var i = 1;
-
-      // // count bikes (with overlappings)
-        // var j = 0;
-
-      for (var z = 0; z < cities.length; z++) {
-
-        city = cities[z];
-
-        if (city === 'frankfurt') {
-
-          lat0 = 49.984826;
-          lon0 = 8.413478;
-          lat1 = 50.239489;
-          lon1 = 8.856560;
-        } else if (city === 'berlin') {
-
-          lat0 = 52.380591;
-          lon0 = 13.010205;
-          lat1 = 52.678389;
-          lon1 = 13.793848;
-        } else
-        // if (city === 'muenchen')
-        {
-
-          lat0 = 48.016406;
-          lon0 = 11.312153;
-          lat1 = 48.256832;
-          lon1 = 11.800645;
-        }
-
-        for (var lat = lat0; lat <= lat1; lat = lat + (1 / 110.574) * 0.9) {
-          // for transformation of 1km to degree see https://stackoverflow.com/questions/1253499/simple-calculations-for-working-with-lat-lon-km-distance
-          for (var lon = lon0; lon <= lon1; lon = lon + (1 / (111.320 * Math.cos(lat * (Math.PI / 180)))) * 0.9) {
-
-            var url = baseUrl + 'latitude=' + lat + '&longitude=' + lon;
-            console.log(city + ': ' + i + ': ' + url);
-
-            setTimeout(req, i * 120, url, city);
-            i++;
-          }
-        }
-      }
+      callback();
     } else {
 
-      console.log(error);
+      console.error(error);
     }
+  })
+}
 
+function prepare(db) {
+  // count requests
+  let i = 1;
 
-    function req(url, city) {
+  for (let city of cities) {
 
-      request(url, {jar:true}, scrape);
+    let boundary = boundaries[city]
 
-      function scrape(error, response, body) {
+    // For transformation of 1km to degree see
+    // https://stackoverflow.com/a/1253545/2037629
+    for (let lat = boundary.lat0; lat <= boundary.lat1; lat = lat + (1 / 110.574) * 0.9) {
 
-        --i;
+      for (let lon = boundary.lon0; lon <= boundary.lon1; lon = lon + (1 / (111.320 * Math.cos(lat * (Math.PI / 180)))) * 0.9) {
 
-        db.collection('requests').insert({
-          url: url,
-          date: date,
-          hour: hour,
-          minute: minute,
-          city: city,
-          statusCode: response.statusCode
-        });
+        const url = `${baseUrl}latitude=${lat}&longitude=${lon}`;
 
-        if (response && response.statusCode === 200) {
+        console.log(`${city}: ${i}: ${url}`);
 
-          var bikes = JSON.parse(body).data.list;
-          // j = j + bikes.length;
-
-          save(bikes, db, city);
-        } else {
-
-          console.log('Request ' + url + ' failed: Status ' + response.statusCode);
-
-          if (i === 1) {
-
-            console.log('Close database');
-            db.close();
-          }
-        }
+        setTimeout(req, i * 120, url, city);
+        i++;
       }
     }
+  }
+}
 
-    function save(data, db, city) {
+function req(url, city) {
 
-      var collection = db.collection(collectionName);
+  request(url, {jar:true}, scrape);
 
-      if (data === undefined) {
+  function scrape(error, response, body) {
 
-        data === [];
-      }
+    --i;
 
-      data.forEach(function (element) {
+    db.collection('requests').insert({
+      url,
+      date,
+      hour,
+      minute,
+      city,
+      statusCode: response.statusCode
+    });
 
-        element['date'] = date;
-        element['hour'] = hour;
-        element['minute'] = minute;
-        element['city'] = city;
+    if (response && response.statusCode === 200) {
 
-        // TODO: bulkOperation und close database in callback
-        collection.update(
-          {
-            id: element.id,
-            date: date,
-            hour: hour,
-            city: city
-          },
-          element,
-          { upsert: true, multi: false },
-          function (error) {
+      const bikes = JSON.parse(body).data.list;
+      // j = j + bikes.length;
 
-            if (error) {
+      save(bikes, db, city);
+    } else {
 
-              console.log(error);
-            }
+      console.log(`Request ${url} failed: Status ${response.statusCode}`);
 
-            // --j;
-            // console.log(i + '-' + j);
-
-            // if (i === 1 && j === 0) {
-
-            //   console.log('Close database');
-            //   db.close();
-            // }
-          }
-        );
-      });
-
-      // console.log(i);
       if (i === 1) {
 
         console.log('Close database');
         db.close();
       }
     }
-  });
-})();
+  }
+}
 
+function save(data, db, city) {
+
+  const collection = db.collection(collectionName);
+
+  if (data === undefined) {
+
+    data === [];
+  }
+
+  data.forEach(element => {
+
+    element['date'] = date;
+    element['hour'] = hour;
+    element['minute'] = minute;
+    element['city'] = city;
+
+    // TODO: bulkOperation und close database in callback
+    collection.update(
+      {
+        id: element.id,
+        date,
+        hour,
+        city
+      },
+      element,
+      { upsert: true, multi: false },
+      error => {
+
+        if (error) {
+
+          console.log(error);
+        }
+
+        // --j;
+        // console.log(i + '-' + j);
+
+        // if (i === 1 && j === 0) {
+
+        //   console.log('Close database');
+        //   db.close();
+        // }
+      }
+    );
+  });
+
+  // console.log(i);
+  if (i === 1) {
+
+    console.log('Close database');
+    db.close();
+  }
+}
