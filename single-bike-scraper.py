4c1cc9115722f61607e13815b6270d8361b4ec2a
Steffen Kühne <stekhn@gmail.com>
2017-11-20 16:46:23 +0100

    Update scraping interval, update list style

--- single-bike-scraper.py
+++ single-bike-scraper.py
@@ -1,13 +1,29 @@
+import random
 import requests
 import time
 import json
 import unicodecsv
 
-outfile = open("single_bike_data.csv","wb")
+outfile = open("single_bike_data_2.csv", "wb")
 
 final_file = unicodecsv.writer(outfile, encoding='utf-8')
-final_file.writerow(
-    ['country','id','success','nolatlon','latitude','longitude','iconUrl','unitMinutes','rideMinutes','hasStation','currency','price','overTime','freeMinutes','promotionActivityType'])
+final_file.writerow([
+  'country',
+  'id',
+  'success',
+  'nolatlon',
+  'latitude',
+  'longitude',
+  'iconUrl',
+  'unitMinutes',
+  'rideMinutes',
+  'hasStation',
+  'currency',
+  'price',
+  'overTime',
+  'freeMinutes',
+  'promotionActivityType'
+])
 
 baseurl = "https://mobile.o.bike/api/v1/bike/"
 
@@ -20,7 +36,7 @@ countries = {
 country = "de"
 country_id = countries[country]
 
-for n in range (0,20000):
+for n in range(0, 40000):
   bikeno = str('{:d}'.format(n).zfill(6))
   bikeid = country_id+bikeno
   url = baseurl+bikeid
@@ -31,10 +47,27 @@ for n in range (0,20000):
       nolatlon = True
     else:
       nolatlon = False
-    final_file.writerow(
-      [country, bikeid, data['success'], nolatlon, data["data"]["latitude"], data["data"]["longitude"], data["data"]["iconUrl"], data["data"]["unitMinutes"], data["data"]["rideMinutes"], data["data"]["hasStation"], data["data"]["currency"], data["data"]["price"], data["data"]["overTime"], data["data"]["freeMinutes"], data["data"]["promotionActivityType"]])
+    final_file.writerow([
+      country,
+      bikeid,
+      data["success"],
+      nolatlon,
+      data["data"]["latitude"],
+      data["data"]["longitude"],
+      data["data"]["iconUrl"],
+      data["data"]["unitMinutes"],
+      data["data"]["rideMinutes"],
+      data["data"]["hasStation"],
+      data["data"]["currency"],
+      data["data"]["price"],
+      data["data"]["overTime"],
+      data["data"]["freeMinutes"],
+      data["data"]["promotionActivityType"]
+    ])
 
   elif data["success"] == False:
     final_file.writerow(
       [country, bikeid, data['success']])
-  time.sleep(1)
+
+  print bikeid
+  time.sleep(random.randint(1, 3))
